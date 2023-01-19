fe20ffbc90b15604d10da123f1d3a5a776c2a2d7
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-19 19:54:20 +0300

    Dynamic showing of movie info on click.

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
+import MovieShow from "./MovieShow";
 import MoviesList from "./MoviesList";
 
+
 function MoviesPage({ movies }) {
+  // useRouteMatch returns a special object with information about
+  // the currently matched route
+  const match = useRouteMatch();
+  console.log(match);
   return (
     <div>
       <MoviesList movies={movies} />
+      {/* 
+        we can use the current URL from the `match` object as part of the path;
+        this will generate a url like "/movies/:movieId"
+      */}
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies}/>
+      </Route>
     </div>
   );
 }
